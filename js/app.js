8b17e7e17da8f4badecf5487c62f39b9c0f15991
Roy Martínez <roymartinezramirez@gmail.com>
2022-03-25 09:03:27 -0500

    add cube swiper

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-window.swiper = new Swiper({
-    el: '.slider__contenedor',
-    slideClass: 'slider__slide',
-    createElements: true,
-    autoplay: {
-        delay: 5000
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
     },
-    loop: true,
-    // spaceBetween: 20,
-    pagination: true,
-    navigation: true
+    pagination: {
+        el: ".swiper-pagination",
+    },
 });
